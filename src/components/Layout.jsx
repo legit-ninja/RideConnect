7a9753ff025b746efeb17c7c80891b6bc38d2752
Jeremy Lee <6729613+legit-ninja@users.noreply.github.com>
2026-07-04 15:03:22 -0400

    Update src/components/Layout.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import SiteHeader from '@/components/SiteHeader';
+import SiteFooter from '@/components/SiteFooter';
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <SiteHeader />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <SiteFooter />
+    </div>
+  );
+}
